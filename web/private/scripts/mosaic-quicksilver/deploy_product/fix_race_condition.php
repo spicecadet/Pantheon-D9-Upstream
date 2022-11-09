03be244f538f20755fcb74a4a8c85e658b2dbf10
Edmund Turbin <edmund.turbin@pantheon.io>
2022-11-09 14:13:34 +0000

    Sleep 30

--- web/private/scripts/mosaic-quicksilver/deploy_product/fix_race_condition.php
+++ web/private/scripts/mosaic-quicksilver/deploy_product/fix_race_condition.php
@@ -4,3 +4,5 @@
  * This file left intentionally empty.  Its just here as a temporary workaround for the problem
  * on the deploy_product "after" stage whereby scripts are started before the container is ready
  */
+
+ sleep(30);
