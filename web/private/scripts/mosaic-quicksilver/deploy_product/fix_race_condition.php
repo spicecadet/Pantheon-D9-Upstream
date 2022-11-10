1fdf188b252483c2dd9871286cfcb0db9d7609e1
Edmund Turbin <edmund.turbin@pantheon.io>
2022-11-10 16:17:44 +0000

    Removed sleep

--- web/private/scripts/mosaic-quicksilver/deploy_product/fix_race_condition.php
+++ web/private/scripts/mosaic-quicksilver/deploy_product/fix_race_condition.php
@@ -4,6 +4,3 @@
  * This file left intentionally empty.  Its just here as a temporary workaround for the problem
  * on the deploy_product "after" stage whereby scripts are started before the container is ready
  */
-$time = 120;
-// sleep($time);
-echo "Waking up after " . $time . " seconds";
