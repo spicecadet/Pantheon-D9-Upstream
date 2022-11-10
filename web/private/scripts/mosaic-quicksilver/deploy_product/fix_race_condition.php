66d8e3e3349cf56d4ddd2169c01210897dd8ee4a
Edmund Turbin <edmund.turbin@pantheon.io>
2022-11-10 11:46:25 +0000

    Sleep for 120

--- web/private/scripts/mosaic-quicksilver/deploy_product/fix_race_condition.php
+++ web/private/scripts/mosaic-quicksilver/deploy_product/fix_race_condition.php
@@ -4,6 +4,6 @@
  * This file left intentionally empty.  Its just here as a temporary workaround for the problem
  * on the deploy_product "after" stage whereby scripts are started before the container is ready
  */
-
- sleep(60);
- echo "Waking up after 60 seconds";
+$time = 120;
+ sleep($time);
+ echo "Waking up after " . $time . " seconds";
